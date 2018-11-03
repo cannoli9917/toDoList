4e6b1270559ce2cb35f8af92da1e2b4898559a6a
cannoli9917 <katie.pennachio@gmail.com>
2018-11-03 15:46:02 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const toDoInput = document.getElementById('toDoInput');
+const addToListButton = document.getElementById('addToListButton');
+const LIST = document.getElementById('LIST');
+
+
+
+function createLi() {
+  var toDoItem = toDoInput.value;
+  var task = document.createElement('li');
+  LIST.appendChild(task);
+  task.innerHTML = toDoItem;
+}
+
+function addToList() {
+  createLi();
+  toDoInput.value = "";
+}
+
+function handleKeyPress(e) {
+  if (e.which === 13) {
+    e.stopPropagation();
+    e.preventDefault();
+    addToListButton.click();
+    return false;
+  }
+}
+
+function init() {
+  addToListButton.addEventListener('click', addToList);
+  toDoInput.addEventListener('keydown', handleKeyPress);
+}
+
+
+
+init();
